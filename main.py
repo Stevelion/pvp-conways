96de2e2dce81801ca50e870e01de06cfdcfa69f7
Stevelion <54511192+Stevelion@users.noreply.github.com>
2022-10-25 18:25:54 -0400

    PvP Conway's Alpha v0.3
    Basic functionality of Conway's Game of Life implemented on a 30x30 finite grid. All cells outside shown grid are treated as permanently dead.

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import numpy as np
 import pygame
+import time
 
 
 # owner is: 0 = dead, 1 = p1, 2 = p2, 3 = shrapnel, 4 = key/what you defend
 
 
 
+
+
+
+
+
 def update_grid():
     new_grid = grid.copy()
     relevants = set()
-    for living in np.nonzero(grid):
-        for cell in grid[living[0]-1:living[0]+1, living[1]-1:living[1]+1]:
-            relevants.add(cell)
+    nonzero = np.nonzero(grid)
+    alive = [(nonzero[0][n], nonzero[1][n]) for n in range(len(nonzero[0]))]
+    for living in alive:
+        for y in range(-1, 2):
+            for x in range(-1, 2):
+                relevants.add((living[0] + y, living[1] + x))
     for cell in relevants:
-        new_grid[cell] = update_cell(grid[cell[0]-1:cell[0]+1, cell[1]-1:cell[1]+1])
+        new_grid[cell] = update_cell(grid[cell[0]-1:cell[0]+2, cell[1]-1:cell[1]+2])
+    # clean up edges
+    for n in range(2):
+        new_grid[n, :] = grid[n, :] * 0
+        new_grid[-n-1, :] = grid[-n-1, :] * 0
+        new_grid[:, n] = grid[:, n] * 0
+        new_grid[:, -n-1] = grid[:, -n-1] * 0
+    return new_grid
+
+
+
 
 def update_cell(kernel):
     if kernel[1,1] == 0: # if dead
-        if len(np.nonzero(kernel)) == 3:
-            # become alive
-            if len(set([kernel[xy] for xy in np.nonzero(kernel)])) == 1: # if all the same, return it
+        if len(np.nonzero(kernel)[0]) == 3: # become alive
+            # if all the same, return it
+            nonzero = np.nonzero(kernel)
+            if len(set([kernel[nonzero[0][n], nonzero[1][n]] for n in range(len(nonzero[0]))])) == 1:
                 return np.max(kernel)
             else: # if mixed
                 return 3
         else: # stay dead
             return 0
     else: # if living
-        if len(np.nonzero(kernel)) in (3,4): # stay alive
+        if len(np.nonzero(kernel)[0]) in (3,4): # stay alive
             return kernel[1,1]
         else: # die
             return 0
@@ -35,51 +55,36 @@ def update_cell(kernel):
 
 
 def handle_game_events(events):
-    global right_drag
-    global middle_drag
     for event in events:
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    right_drag = False # safety to catch hitting space without mouseup
-                    time = not time # flip time on space press
-                if event.key == pygame.K_ESCAPE:
-                    middle_drag = False
-                    right_drag = False # safety to catch hitting space without mouseup
-                    open_escape()
-            if event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN) and event.button < 4:
-                handle_game_click(event)
-            if event.type == pygame.MOUSEMOTION:
-                pass # handle motion
+        if event.type == pygame.MOUSEMOTION:
+            pass # handle motion
+        elif event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN) and event.button in (1, 3):
+            handle_game_click(event)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                global time_on
+                time_on = not time_on # flip time_on on space press
+            elif event.key == pygame.K_ESCAPE:
+                open_escape()
+        elif event.type == pygame.QUIT:
+            global running
+            running = False
 
 
 def handle_game_click(click):
     """Find click target and execute"""
     mouse_x, mouse_y = click.pos
-    global right_drag
-    global middle_drag
     # determine what was clicked
-    if click.button == 3:
-        middle_drag = not middle_drag
-    elif middle_drag == True: # stop dragging if there is a click
-        middle_drag = False
-        return
     # what to do if time is stopped
-    if not time:
-        if click.button == 2 and click.type == pygame.MOUSEBUTTONUP:
-            right_drag = False
-        # if click is on grid
-        if border_width > mouse_x < border_width + grid.shape[1] - 4 * cell_size and border_width > mouse_y < border_width + grid.shape[0] - 4 * cell_size:
-            if click.button == 1 and click.type == pygame.MOUSEBUTTONUP: # when left click
-                click_grid([(axis - border_width)/cell_size for axis in click.pos])
-            if click.button == 2 and click.type == pygame.MOUSEBUTTONDOWN: # when right click down
-                right_drag = True
-
-
-def click_grid(coords):
-    """If grid is clicked, find which cell and operate on that cell"""
-    coords = coords[::-1] # reversed because numpy uses reversed coords
-    if grid[coords] == 0: grid[coords] = 1
-    else: grid[coords] = 0
+    if not time_on:
+        # if click is within grid bounds
+        if border_width < mouse_x < border_width + (grid.shape[1] - 4) * cell_size and border_width < mouse_y < border_width + (grid.shape[0] - 4) * cell_size:
+            coords = [int((axis - border_width)/cell_size + 2) for axis in click.pos[::-1]]
+            if click.type == pygame.MOUSEBUTTONDOWN and click.button == 1 and grid[coords[0], coords[1]] == 0: # when left click
+                grid[coords[0], coords[1]] = 1
+            if click.type == pygame.MOUSEBUTTONDOWN and click.button == 3:
+                if grid[coords[0], coords[1]] == 1 or debug:
+                    grid[coords[0], coords[1]] = 0
 
 
 def open_escape():
@@ -88,31 +93,64 @@ def open_escape():
 
 
 def draw_grid():
-    pass
+    for x in range(grid.shape[1] - 4):
+        for y in range(grid.shape[0] - 4):
+            draw_x = x * cell_size + border_width
+            draw_y = y * cell_size + border_width
+            rect = pygame.Rect(draw_x, draw_y, cell_size - 1, cell_size - 1)
+            pygame.draw.rect(window, colours[grid[y + 2, x + 2]], rect)
 
 
+def init_vars():
+    """initialize some vars, mostly here for alpha construction"""
+    global time_on
+    time_on = False
+    global cell_size
+    cell_size = 20 # pixels
+    global border_width
+    border_width = 20 # pixels
+    global running
+    running = True
+    global tickrate
+    tickrate = 0.3 # seconds
+    global debug
+    debug = True
+    global colours
+    colours = {0 : (0, 0, 0), 1 : (0, 0, 255), 2 : (255, 0, 0), 3 : (225, 115, 20), 4 : (140, 0, 200)}
 
 
 def main():
     # start a game
 
-    # initialize some vars
-    global time
-    time = False
-    global right_drag
-    right_drag = False
-    global middle_drag
-    middle_drag = False
-    global cell_size
-    cell_size = 20
-    global border_width
-    border_width = 20
+    init_vars()
 
-    gamestate = 'ingame'
 
+    WINDOW_WIDTH = 1000
+    WINDOW_HEIGHT = 700
+    global window
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    window.fill((240, 240, 240)) # white background
+
+    grid_size = 30
+    grid_size += 4
     global grid
-    grid = np.zeros(34,34)
-    while gamestate == 'ingame':
+    grid = np.zeros((grid_size, grid_size))
+
+    start_time = time.perf_counter()
+    cycles = 1
+    while running:
         # get events
         handle_game_events(pygame.event.get())
+
+        if time.perf_counter() - start_time - cycles * tickrate > 0:
+            cycles += 1
+            if time_on:
+                grid = update_grid()
+
         draw_grid()
+        pygame.display.update()
+
+        time.sleep(0.01)
+
+
+main()
