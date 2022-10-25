97f7a671293945dad1c9d84936610b5362a7aa65
Stevelion <54511192+Stevelion@users.noreply.github.com>
2022-10-25 11:59:55 -0400

    PvP Conway v0.2
    Progress on event handling and reworked some common function vars as global vars

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ def update_cell(kernel):
             return 0
 
 
-
-def update_grid(grid):
+def update_grid():
     new_grid = grid.copy()
     relevants = set()
     for living in np.nonzero(grid):
@@ -35,21 +34,47 @@ def update_grid(grid):
         new_grid[cell] = update_cell(grid[cell[0]-1:cell[0]+1, cell[1]-1:cell[1]+1])
 
 
-# handle_click and find_cell are wip attempts at translating clicks to interactions with a cell
-def handle_click(click, grid, cell_size = 20, border_width = 20):
-    mouse_x, mouse_y = click.pos
-    # determine what was clicked
-    # logic to find grid clicks
-    pass
 
-def find_cell(click, grid_dimensions, cell_size = 20, border_width = 20):
+
+def click_grid(coords):
+    """If grid is clicked, find which cell and operate on that cell"""
+    coords = coords[::-1] # reversed because numpy uses reversed coords
+    if grid[coords] == 0: grid[coords] = 1
+    else: grid[coords] = 0
+
+
+def handle_click(click):
+    """Find click target and execute"""
     mouse_x, mouse_y = click.pos
-    grid_y, grid_x = grid_dimensions[0] - 4, grid_dimensions[1] - 4
-    if border_width > mouse_x < border_width + grid_x * cell_size and border_width > mouse_y < border_width + grid_y * cell_size:
-        cell_x = ((mouse_x - border_width) / cell_size) - 1
-        cell_y = ((mouse_y - border_width) / cell_size) - 1
-        return (cell_x, cell_y)
-    return
+    global right_drag
+    # determine what was clicked
+    if not time: # what to do if time is stopped
+        if click.button == 2 and click.type == pygame.MOUSEBUTTONUP:
+            right_drag = False
+        # if click is on grid
+        if border_width > mouse_x < border_width + grid.shape[1] - 4 * cell_size and border_width > mouse_y < border_width + grid.shape[0] - 4 * cell_size:
+            if click.button == 1 and click.type == pygame.MOUSEBUTTONUP: # when left click
+                click_grid([(axis - border_width)/cell_size for axis in click.pos])
+            if click.button == 2 and click.type == pygame.MOUSEBUTTONDOWN: # when right click down
+                right_drag = True
+
+
+def handle_game_events(events):
+    for event in events:
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    time = not time # flip time on space press
+                    global right_drag
+                    right_drag = False # safety to catch hitting space without mouseup
+                if event.key == pygame.K_ESCAPE:
+                    pass # open escape menu
+            if event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN):
+                handle_click(event)
+            if event.type == pygame.MOUSEMOTION:
+                pass
+
+
+
 
 
 def draw_grid():
@@ -59,16 +84,23 @@ def draw_grid():
 
 
 def main():
-    grid = np.zeros(34,34)
-    owners = {0:'', 1:'', 2:'', 3:'', 4:''}
+    # start a game
+
+    # initialize some vars
+    global time
+    time = False
+    global right_drag
+    right_drag = False
+    global cell_size
+    cell_size = 20
+    global border_width
+    border_width = 20
+
     gamestate = 'ingame'
-    run = True
-    while run:
+
+    global grid
+    grid = np.zeros(34,34)
+    while gamestate == 'ingame':
         # get events
-        ev = pygame.event.get()
-        for event in ev:
-            if event.type == pygame.MOUSEBUTTONUP:
-                target = find_cell(event, grid.shape)
-                if grid[target] == 0: grid[target] = 1
-                else: grid[target] = 0
+        handle_game_events(pygame.event.get())
         draw_grid()
