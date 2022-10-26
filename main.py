ed1a4a96954a667a98e5ff8a4df0e0b75f28e2b2
Stevelion <54511192+Stevelion@users.noreply.github.com>
2022-10-26 10:25:45 -0400

    PvP Conway's Alpha v0.4
    Bashed together level editor, changing modes is currently handled by commenting out code at the bottom of the script

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ import time
 # owner is: 0 = dead, 1 = p1, 2 = p2, 3 = shrapnel, 4 = key/what you defend
 
 
-
-
-
+def init_vars():
+    """initialize some vars, mostly here for alpha construction"""
+    global cell_size, border_width, tickrate, colours
+    cell_size = 20 # pixels
+    border_width = 30 # pixels
+    tickrate = 0.1 # seconds
+    colours = {0 : (0, 0, 0), 1 : (0, 0, 255), 2 : (255, 0, 0), 3 : (225, 115, 20), 4 : (140, 0, 200)}
 
 
 
 def update_grid():
+    """handle updating the grid with CGoL logic every game tick while time is on"""
+    global new_grid
     new_grid = grid.copy()
     relevants = set()
     nonzero = np.nonzero(grid)
@@ -32,9 +38,10 @@ def update_grid():
     return new_grid
 
 
-
-
 def update_cell(kernel):
+    """basic CGoL logic for a cell
+    kernel refers to the 3x3 of cells around the currently focused cell"""
+    global game_over
     if kernel[1,1] == 0: # if dead
         if len(np.nonzero(kernel)[0]) == 3: # become alive
             # if all the same, return it
@@ -42,6 +49,8 @@ def update_cell(kernel):
             if len(set([kernel[nonzero[0][n], nonzero[1][n]] for n in range(len(nonzero[0]))])) == 1:
                 return np.max(kernel)
             else: # if mixed
+                if kernel[1,1] == 4:
+                    game_over = True
                 return 3
         else: # stay dead
             return 0
@@ -49,33 +58,36 @@ def update_cell(kernel):
         if len(np.nonzero(kernel)[0]) in (3,4): # stay alive
             return kernel[1,1]
         else: # die
+            if kernel[1,1] == 4:
+                game_over = True
             return 0
 
 
 
 
 def handle_game_events(events):
+    """top level function to delegate events"""
     for event in events:
-        if event.type == pygame.MOUSEMOTION:
-            pass # handle motion
-        elif event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN) and event.button in (1, 3):
+        if event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN) and event.button in (1, 3):
             handle_game_click(event)
-        elif event.type == pygame.KEYUP:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                global time_on
-                time_on = not time_on # flip time_on on space press
+                if not game_over:
+                    global time_on
+                    time_on = not time_on # flip time_on on space press
             elif event.key == pygame.K_ESCAPE:
-                open_escape()
+                global ingame
+                ingame = False
         elif event.type == pygame.QUIT:
-            global running
-            running = False
+            pygame.quit()
+            quit()
 
 
 def handle_game_click(click):
     """Find click target and execute"""
     mouse_x, mouse_y = click.pos
     # determine what was clicked
-    # what to do if time is stopped
+    # only do if time is stopped
     if not time_on:
         # if click is within grid bounds
         if border_width < mouse_x < border_width + (grid.shape[1] - 4) * cell_size and border_width < mouse_y < border_width + (grid.shape[0] - 4) * cell_size:
@@ -83,16 +95,18 @@ def handle_game_click(click):
             if click.type == pygame.MOUSEBUTTONDOWN and click.button == 1 and grid[coords[0], coords[1]] == 0: # when left click
                 grid[coords[0], coords[1]] = 1
             if click.type == pygame.MOUSEBUTTONDOWN and click.button == 3:
-                if grid[coords[0], coords[1]] == 1 or debug:
+                if grid[coords[0], coords[1]] == 1:
                     grid[coords[0], coords[1]] = 0
 
 
 def open_escape():
+    """open menu on esc key press"""
     pass
 
 
 
 def draw_grid():
+    """draw visible grid on pygame window"""
     for x in range(grid.shape[1] - 4):
         for y in range(grid.shape[0] - 4):
             draw_x = x * cell_size + border_width
@@ -101,44 +115,21 @@ def draw_grid():
             pygame.draw.rect(window, colours[grid[y + 2, x + 2]], rect)
 
 
-def init_vars():
-    """initialize some vars, mostly here for alpha construction"""
-    global time_on
+
+def game(board = np.zeros((34, 34))):
+    
+    """start a game"""
+    global ingame, game_over, time_on
+    ingame = True
+    game_over = False
     time_on = False
-    global cell_size
-    cell_size = 20 # pixels
-    global border_width
-    border_width = 20 # pixels
-    global running
-    running = True
-    global tickrate
-    tickrate = 0.3 # seconds
-    global debug
-    debug = True
-    global colours
-    colours = {0 : (0, 0, 0), 1 : (0, 0, 255), 2 : (255, 0, 0), 3 : (225, 115, 20), 4 : (140, 0, 200)}
 
-
-def main():
-    # start a game
-
-    init_vars()
-
-
-    WINDOW_WIDTH = 1000
-    WINDOW_HEIGHT = 700
-    global window
-    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    window.fill((240, 240, 240)) # white background
-
-    grid_size = 30
-    grid_size += 4
     global grid
-    grid = np.zeros((grid_size, grid_size))
+    grid = board
 
     start_time = time.perf_counter()
     cycles = 1
-    while running:
+    while ingame:
         # get events
         handle_game_events(pygame.event.get())
 
@@ -146,11 +137,92 @@ def main():
             cycles += 1
             if time_on:
                 grid = update_grid()
+                if game_over: time_on = False
 
         draw_grid()
         pygame.display.update()
 
         time.sleep(0.01)
+
+
+
+def level_edit():
+    """quickly bashed together level editor that prints grid to console"""
+    global ingame, saved_grid
+    ingame = True
+    global grid
+    grid = np.zeros((34, 34))
+    while ingame:
+        # get events
+        events = pygame.event.get()
+        for event in events:
+            if event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN) and event.button in (1, 3):
+                mouse_x, mouse_y = event.pos
+                # if click is within grid bounds
+                if border_width < mouse_x < border_width + (grid.shape[1] - 4) * cell_size and border_width < mouse_y < border_width + (grid.shape[0] - 4) * cell_size:
+                    coords = [int((axis - border_width)/cell_size + 2) for axis in event.pos[::-1]]
+                    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and grid[coords[0], coords[1]] < 4: # increment when left click
+                        grid[coords[0], coords[1]] += 1
+                    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3 and grid[coords[0], coords[1]] > 0: # decrement when right click
+                        grid[coords[0], coords[1]] -= 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    ingame = False
+                elif event.key == pygame.K_p:
+                    np.savetxt('savedgrid.csv', grid, '%1.0f', delimiter=",")
+            elif event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        draw_grid()
+        pygame.display.update()
+        time.sleep(0.01)
+
+
+
+
+def draw_main_menu():
+    window.fill((240, 240, 240))
+    pygame.display.update()
+
+
+def draw_level_select():
+    window.fill((240, 240, 240))
+    pygame.display.update()
+
+
+def menu():
+    # draw main menu
+    draw_main_menu()
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type in (pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN) and event.button in (1, 3):
+                # logic to find click target
+                game()
+                draw_main_menu()
+                
+            elif event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        time.sleep(0.01)
+
+
+
+
+def main():
+    init_vars()
+    WINDOW_WIDTH = 660
+    WINDOW_HEIGHT = 660
+    global window
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    window.fill((240, 240, 240)) # white background
+
+    
+    
+    # level_edit()
+    board = np.genfromtxt('levels/demo.csv', delimiter=',')
+    # board = np.genfromtxt('levels/basic_level.csv', delimiter=',')
+    game(board)
 
 
 main()
