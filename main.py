caf16bd324bf9851ad02fbe60342c52af7fdb13e
Stevelion <54511192+Stevelion@users.noreply.github.com>
2022-10-28 18:27:04 -0400

    PvP Conway's Alpha v1.3
    Added LifeButton and LifeMenu abstract classes and used them for basic graphics for main menu buttons

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ cell_size = 20 # pixels
 border_width = 30 # pixels
 tickrate = 0.1 # seconds
 # owner: 0 = dead, 1 = player, 2 = enemy, 3 = shrapnel, 4 = what to defend/attack
-colours = {0 : (0, 0, 0), 1 : (0, 0, 255), 2 : (255, 0, 0), 3 : (225, 115, 20), 4 : (140, 0, 200)}
+COLOURS = {0 : (0, 0, 0), 1 : (0, 0, 255), 2 : (255, 0, 0), 3 : (225, 115, 20), 4 : (140, 0, 200)}
 BUILD_COLOUR = (30, 240, 80)
 DEFAULT_BUTTON_COLOUR = (180,180,180)
+BACKGROUND_COLOUR = (240,240,240)
 
 
 
@@ -23,21 +24,21 @@ class Grid:
         """handle updating the grid with CGoL logic every game tick while time is on"""
         self.new_array = self.array.copy()
         relevants = set()
-        nonzero = np.nonzero(self.array)
-        alive = [(nonzero[0][n], nonzero[1][n]) for n in range(len(nonzero[0]))]
-        for living in alive:
+        nonzero = np.nonzero(self.array) # find living (nonzero) cells
+        alive = [(nonzero[0][n], nonzero[1][n]) for n in range(len(nonzero[0]))] # convert np.nonzero output into list of coordinate tuples
+        for living in alive: # find all cells adjacent to a living cell
             for y in range(-1, 2):
                 for x in range(-1, 2):
                     relevants.add((living[0] + y, living[1] + x))
         for cell in relevants:
-            self.new_array[cell] = self.update_cell(self.array[cell[0]-1:cell[0]+2, cell[1]-1:cell[1]+2])
+            self.new_array[cell] = self.update_cell(self.array[cell[0]-1:cell[0]+2, cell[1]-1:cell[1]+2]) # pass 3x3 around cell to update_cell()
         # clean up edges
         for n in range(2):
             self.new_array[n, :] = self.array[n, :] * 0
             self.new_array[-n-1, :] = self.array[-n-1, :] * 0
             self.new_array[:, n] = self.array[:, n] * 0
             self.new_array[:, -n-1] = self.array[:, -n-1] * 0
-        return self.new_array
+        self.array = self.new_array
 
     def update_cell(self, kernel):
         """basic CGoL logic for a cell
@@ -74,22 +75,20 @@ class Game:
         self.time_on = False
         self.game_over = False
         self.ingame = True
-        self.start_time = time.perf_counter()
         self.cycles = 1
 
     def main(self):
-        self.surface.fill((240, 240, 240))
+        self.start_time = time.perf_counter()
+        self.surface.fill(BACKGROUND_COLOUR)
         pygame.draw.rect(self.surface, BUILD_COLOUR, self.build_area)
         while self.ingame:
             # get events
             self.handle_events(pygame.event.get())
-
             if time.perf_counter() - self.start_time - self.cycles * tickrate > 0: # true every 'tickrate' seconds
                 self.cycles += 1
                 if self.time_on: # only run update logic if time is turned on
-                    self.grid.array = self.grid.update_grid()
+                    self.grid.update_grid()
                     if self.game_over: self.time_on = False # stop time permanently on game end
-
             self.draw_grid()
             time.sleep(0.01) # events will rarely happen multiple times in 1/100 of a second
     
@@ -129,7 +128,7 @@ class Game:
                 draw_x = x * cell_size + border_width
                 draw_y = y * cell_size + border_width
                 rect = pygame.Rect(draw_x, draw_y, cell_size - 1, cell_size - 1)
-                pygame.draw.rect(self.surface, colours[self.grid.array[y + 2, x + 2]], rect)
+                pygame.draw.rect(self.surface, COLOURS[self.grid.array[y + 2, x + 2]], rect)
         pygame.display.update()
 
             
@@ -142,8 +141,9 @@ class LevelEditor(Game):
         Game.__init__(self, surface, grid, build_area)
 
     def main(self):
-        self.surface.fill((240, 240, 240))
+        self.surface.fill(BACKGROUND_COLOUR)
         pygame.draw.rect(self.surface, BUILD_COLOUR, self.build_area)
+        self.start_time = time.perf_counter()
         while self.ingame:
             # get events
             self.handle_events(pygame.event.get())
@@ -151,7 +151,7 @@ class LevelEditor(Game):
             if time.perf_counter() - self.start_time - self.cycles * tickrate > 0: # true every 'tickrate' seconds
                 self.cycles += 1
                 if self.time_on: # only run update logic if time is turned on
-                    self.grid.array = self.grid.update_grid()
+                    self.grid.update_grid()
             self.draw_grid()
             time.sleep(0.01)
 
@@ -189,74 +189,66 @@ class LevelEditor(Game):
 
 
 
-class Button(): # parent class for menu buttons
-    def __init__(self, name, rect, colour = DEFAULT_BUTTON_COLOUR):
+class LifeButton():
+    """Abstract class for buttons that collapse into Conway's Game of Life sims when hovered"""
+    def __init__(self, name, rect, grid, background = DEFAULT_BUTTON_COLOUR):
         self.name = name
         self.rect = pygame.Rect(rect)
-        self.colour = colour
-        pygame.font.init() # initialize and set font for label, will be removed later and replaced with graphics
-        self.font = pygame.font.SysFont('Arial', 25)
+        self.grid = Grid(grid, self)
+        self.background = background
+        self.game_over = False
+        self.hovered = False
+        self.cell_size = 5
+        self.colours = COLOURS
+    
+    def function(self):
+        self.hovered = False
+        self.grid.array = self.placeholder_grid
+
+    def update(self):
+        self.grid.update_grid()
+    
+    def hover(self, event):
+        if not self.hovered and self.rect.collidepoint(event.pos): # if it wasn't hovered and now is
+            self.hovered = True
+            self.placeholder_grid = self.grid.array
+        elif self.hovered and not self.rect.collidepoint(event.pos): # if it was hovered and now isn't
+            self.hovered = False
+            self.grid.array = self.placeholder_grid
 
     def draw(self, surface):
-        pygame.draw.rect(surface, self.colour, self.rect)
-        surface.blit(self.font.render(self.name, True, (0,0,0)), self.rect[0:2])
-
-
-class LevelSelectButton(Button):
-    def __init__(self, name, rect, colour=DEFAULT_BUTTON_COLOUR):
-        Button.__init__(self, name, rect, colour)
-
-    def function(self): # function to execute when button is clicked
-        level_select.main()
-
-
-class LevelEditorButton(Button):
-    def __init__(self, name, rect, colour=DEFAULT_BUTTON_COLOUR):
-        Button.__init__(self, name, rect, colour)
-
-    def function(self): # function to execute when button is clicked
-        level_editor = LevelEditor(window, np.zeros((34, 34)), pygame.Rect(border_width + cell_size*15, border_width, cell_size*15 - 1, cell_size*15 - 1))
-        level_editor.main()
-    
-
-class EmptyLevelButton(Button):
-    def __init__(self, name, rect, colour=DEFAULT_BUTTON_COLOUR):
-        Button.__init__(self, name, rect, colour)
-
-    def function(self): # function to execute when button is clicked
-        game = Game(window, np.zeros((34, 34)), pygame.Rect(border_width, border_width, cell_size*30 - 1, cell_size*30 - 1))
-        game.main()
-
-
-class LevelButton(Button): # child class for buttons in level select submenu
-    def __init__(self, name, rect, filename, colour = DEFAULT_BUTTON_COLOUR):
-        Button.__init__(self, name, rect, colour)
-        self.filename = filename
-        
-    def function(self): # function to execute when button is clicked
-        if self.filename == None: return
-        game = Game(window , np.genfromtxt('levels/' + self.filename, delimiter=','), pygame.Rect(border_width + cell_size*15, border_width, cell_size*15 - 1, cell_size*15 - 1))
-        game.main()
+        for x in range(self.grid.array.shape[1] - 10):     # draw rectangles for each xy pair
+            for y in range(self.grid.array.shape[0] - 10): # will need to be reworked if screen is made dragable
+                draw_x = x * self.cell_size + self.rect[0]
+                draw_y = y * self.cell_size + self.rect[1]
+                rect = pygame.Rect(draw_x, draw_y, self.cell_size, self.cell_size)
+                pygame.draw.rect(surface, self.colours[self.grid.array[y + 5, x + 5]], rect)
 
 
 
-
-class Menu:
-    """Abstract menu class"""
-    def __init__(self, surface, buttons):
-        self.buttons = buttons
+class LifeMenu:
+    """Abstract menu class for containing and updating LifeButtons"""
+    def __init__(self, surface, background, buttons):
         self.surface = surface
+        self.buttons = buttons
+        self.background = background
+        self.tickrate = 0.1
 
     def main(self): # main event loop
+        self.restart_timer()
         self.draw()
         while True:
             events = pygame.event.get()
             for event in events:
+                if event.type == pygame.MOUSEMOTION: # find if mouse motion involved hovering or unhovering a button
+                    for button in self.buttons:
+                        button.hover(event)
                 if event.type == pygame.MOUSEBUTTONUP and event.button in (1, 3):
                     # find button and execute it's function
                     for button in self.buttons:
                         if button.rect.collidepoint(event.pos):
                             button.function()
+                    self.restart_timer()
                     self.draw()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
@@ -264,40 +256,106 @@ class Menu:
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+            if time.perf_counter() - self.start_time - self.cycles * self.tickrate > 0: # true every 'tickrate' seconds
+                self.cycles += 1
+                for button in self.buttons:
+                    if button.hovered:
+                        button.update()
+                self.draw()
             time.sleep(0.01)
+    
+    def restart_timer(self):
+        """restart update timer"""
+        self.start_time = time.perf_counter()
+        self.cycles = 1
 
     def draw(self):
         """draw menu with buttons"""
-        self.surface.fill((240, 240, 240))
+        self.surface.fill(self.background)
         for button in self.buttons:
             button.draw(self.surface)
         pygame.display.update()
 
 
-class MainMenu(Menu):
+
+
+class LevelSelectButton(LifeButton):
+    def __init__(self):
+        name = 'Singleplayer'
+        rect = (195,170,270,55)
+        grid = np.genfromtxt('buttons/singleplayer.csv', delimiter=',')
+        LifeButton.__init__(self, name, rect, grid)
+
+    def function(self): # function to execute when button is clicked
+        LifeButton.function(self)
+        level_select.main()
+
+
+class LevelEditorButton(LifeButton):
+    def __init__(self):
+        name = 'Level Editor'
+        rect = (195,255,270,55)
+        grid = np.genfromtxt('buttons/level_editor.csv', delimiter=',')
+        LifeButton.__init__(self, name, rect, grid)
+
+    def function(self): # function to execute when button is clicked
+        LifeButton.function(self)
+        level_editor = LevelEditor(window, np.zeros((34, 34)), pygame.Rect(border_width + cell_size*15, border_width, cell_size*15 - 1, cell_size*15 - 1))
+        level_editor.main()
+    
+
+class EmptyLevelButton(LifeButton):
+    def __init__(self):
+        name = 'Sandbox'
+        rect = (195,340,270,55)
+        grid = np.genfromtxt('buttons/empty_level.csv', delimiter=',')
+        LifeButton.__init__(self, name, rect, grid)
+
+    def function(self): # function to execute when button is clicked
+        LifeButton.function(self)
+        game = Game(window, np.zeros((34, 34)), pygame.Rect(border_width, border_width, cell_size*30 - 1, cell_size*30 - 1))
+        game.main()
+
+
+class LevelButton(LifeButton): # child class for buttons in level select submenu
+    def __init__(self, name, rect, filename):
+        grid = np.zeros((15,26))
+        LifeButton.__init__(self, name, rect, grid)
+        self.filename = filename
+        
+    def function(self): # function to execute when button is clicked
+        LifeButton.function(self)
+        if self.filename == None: return
+        game = Game(window , np.genfromtxt('levels/' + self.filename, delimiter=','), pygame.Rect(border_width + cell_size*15, border_width, cell_size*15 - 1, cell_size*15 - 1))
+        game.main()
+
+
+
+
+class MainMenu(LifeMenu):
     def __init__(self, surface):
-        Menu.__init__(self, surface, self.init_buttons())
+        LifeMenu.__init__(self, surface, (0,0,0), self.init_buttons())
 
     def init_buttons(self): # creates all buttons in the menu and returns them
         return (
-            LevelSelectButton('Singleplayer', (280,200,100,50)),
-            LevelEditorButton('Level Editor', (280,260,100,50)),
-            EmptyLevelButton('Sandbox', (280,320,100,50))
+            LevelSelectButton(),
+            LevelEditorButton(),
+            EmptyLevelButton()
         )
 
 
-class LevelSelect(Menu):
+class LevelSelect(LifeMenu):
     def __init__(self, surface):
-        Menu.__init__(self, surface, self.init_buttons())
+        LifeMenu.__init__(self, surface, BACKGROUND_COLOUR, self.init_buttons())
 
     def init_buttons(self): # creates all buttons in the menu and returns them
         return (
-            LevelButton('Demo', (200,200,100,50), 'demo.csv'),
-            LevelButton('Level 1', (200,260,100,50), 'level_1.csv'),
-            LevelButton('Level 2', (200,320,100,50), 'level_2.csv'),
-            LevelButton('Level 3', (360,200,100,50), 'level_3.csv'),
-            LevelButton('Unused', (360,260,100,50), None),
-            LevelButton('Unused', (360,320,100,50), None)
+            LevelButton('Demo', (200,200,110,55), 'demo.csv'),
+            LevelButton('Level 1', (200,320,110,55), 'level_1.csv'),
+            LevelButton('Level 2', (200,430,110,55), 'level_2.csv'),
+            LevelButton('Level 3', (350,200,110,55), 'level_3.csv'),
+            LevelButton('Unused', (350,320,110,55), None),
+            LevelButton('Unused', (350,430,110,55), None)
         )
 
 
@@ -305,7 +363,7 @@ class LevelSelect(Menu):
 WINDOW_WIDTH = 660
 WINDOW_HEIGHT = 660
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-window.fill((240, 240, 240)) # white background
+window.fill(BACKGROUND_COLOUR) # white background
 
 level_select = LevelSelect(window)
 main_menu = MainMenu(window)
